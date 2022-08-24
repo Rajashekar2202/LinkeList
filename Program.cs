f597993f3714cf784eb77c76be2676048efb8702
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-24 17:51:10 +0530

    added 30 and 56 to 70

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedListProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkList list = new LinkList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.Display();
+        }
+    }
+}
