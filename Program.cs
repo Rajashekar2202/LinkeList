6cd624b9912f8bc5fb6d24171c09f73cd417f0c8
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-24 18:41:20 +0530

    searching the element

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LinkedListProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkList list = new LinkList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.WriteLine(" ");
+            Console.WriteLine("---------------------");
+
+            list.Search(30);
+            list.Display();
+        }
+    }
+}
